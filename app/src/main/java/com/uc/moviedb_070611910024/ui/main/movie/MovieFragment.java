1bd2f593af8f55bae4240e0a5a67838b69f3e4e2
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2020-10-25 05:26:42 +0700

    1

--- app/src/main/java/com/uc/moviedb_070611910024/ui/main/movie/MovieFragment.java
+++ app/src/main/java/com/uc/moviedb_070611910024/ui/main/movie/MovieFragment.java
@@ -2,16 +2,30 @@ package com.uc.moviedb_070611910024.ui.main.movie;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
+import android.text.method.MovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.uc.moviedb_070611910024.R;
+import com.uc.moviedb_070611910024.ui.main.detail.DetailFragmentDirections;
+import com.uc.moviedb_070611910024.ui.splash.SplashFragmentDirections;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MovieFragment extends Fragment {
+
+    @BindView(R.id.btn_movie)
+    Button button;
 
 
 
@@ -25,5 +39,17 @@ public class MovieFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        button.setOnClickListener(view1 -> {
+            NavDirections action = MovieFragmentDirections.actionMovieFragmentToDetailFragment();
+            Navigation.findNavController(view).navigate(action);
+        });
     }
 }
--- app/src/main/java/com/uc/moviedb_070611910024/ui/splash/SplashFragment.java
+++ app/src/main/java/com/uc/moviedb_070611910024/ui/splash/SplashFragment.java
@@ -2,15 +2,27 @@ package com.uc.moviedb_070611910024.ui.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.uc.moviedb_070611910024.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SplashFragment extends Fragment {
+
+    @BindView(R.id.btn_movie)
+    Button button;
+
 
     public SplashFragment() {
         // Required empty public constructor
@@ -22,5 +34,17 @@ public class SplashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        button.setOnClickListener(view1 -> {
+            NavDirections action = SplashFragmentDirections.actionSplashFragmentToMovieFragment();
+            Navigation.findNavController(view).navigate(action);
+        });
+
     }
 }
