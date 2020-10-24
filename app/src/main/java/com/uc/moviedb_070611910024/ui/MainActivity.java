136ace636cc91623253d3837d88cf3dff6a24df8
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2020-10-25 03:31:26 +0700

    Initial commit

---
+++ app/src/main/java/com/uc/moviedb_070611910024/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.uc.moviedb_070611910024.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uc.moviedb_070611910024.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
